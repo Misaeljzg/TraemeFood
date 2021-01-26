e38b332e4ba5f996df01c01458e3d6a181aadb29
Misaeljzg <misaeljzg@gmail.com>
2021-01-25 20:22:39 -0600

    Created DAO

--- app/src/main/java/com/misaeljzg/traemefood/roomdatabase/OrdenDatabase.kt
+++ app/src/main/java/com/misaeljzg/traemefood/roomdatabase/OrdenDatabase.kt
@@ -1,2 +1,35 @@
 package com.misaeljzg.traemefood.roomdatabase
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Orden::class], version = 1, exportSchema = false)
+abstract class OrdenDatabase : RoomDatabase(){
+
+    abstract val ordenDAO: OrdenDAO
+
+    companion object{
+        @Volatile
+        private var INSTANCE: OrdenDatabase? = null
+
+        fun getInstance(context: Context) : OrdenDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+
+                if(instance == null){
+                    instance = Room.databaseBuilder(context.applicationContext,
+                        OrdenDatabase::class.java,
+                        "orden_history_database")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+
+}
+
